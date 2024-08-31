51e37d5995a1d30f5af04d623be271a44904b21d
Rahul <rahul25062@gmail.com>
2024-08-31 12:41:03 +0530

    .

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,19 +2,41 @@
 
 
 // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey:import.meta.env.VITE__FIREBASE_API_KEY,
+//   authDomain: "mern-blog-4ead2.firebaseapp.com",
+//   projectId: "mern-blog-4ead2",
+//   storageBucket: "mern-blog-4ead2.appspot.com",
+//   messagingSenderId: "857339328366",
+//   appId: "1:857339328366:web:48a68cf519b6f2e78b337f"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+  
   apiKey:import.meta.env.VITE__FIREBASE_API_KEY,
-  authDomain: "mern-blog-4ead2.firebaseapp.com",
-  projectId: "mern-blog-4ead2",
-  storageBucket: "mern-blog-4ead2.appspot.com",
-  messagingSenderId: "857339328366",
-  appId: "1:857339328366:web:48a68cf519b6f2e78b337f"
+
+  authDomain: "task-manager-d9b92.firebaseapp.com",
+  projectId: "task-manager-d9b92",
+  storageBucket: "task-manager-d9b92.appspot.com",
+  messagingSenderId: "76269058889",
+  appId: "1:76269058889:web:fbfd2e28d96df54f87f898"
 };
 
-// Initialize Firebase
+// // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
